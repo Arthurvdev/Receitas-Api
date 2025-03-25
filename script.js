5ca426f85b66c524e5ae757cc3f753f62e51baad
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2025-03-25 10:49:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 async function receitas() {
-    const response = await fetch(`https://api-receitas-iota.vercel.app/receitas`,{
-        method: 'GET',
-        mode: 'no-cors'
-    });
+    const response = await fetch(`https://api-receitas-iota.vercel.app/receitas`);
     const data = await response.json();
    console.log(data)
 }
