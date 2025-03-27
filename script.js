3f94ce87dcab4e93ff35426c1dbe8c1061a3bfff
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2025-03-27 11:05:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ async function receitas() {
                 <img class="receita_img" src="${receita.url_imagem}" alt="${receita.nome}">
                 <h2 class="receita_h2">${receita.nome}</h2>
                 <p class="receita_p"><strong>Ingredientes:</strong> ${receita.ingredientes}</p>
-                <p class="receita_p"><strong>Tempo de Preparo:</strong> ${receita.tempo_preparo}</p>
+                <p class="receita_p"><strong>Tempo de Preparo:</strong> ${receita.tempo_de_preparo}</p>
                 <p class="receita_p"><strong>Modo de Preparo:</strong> ${receita.modo_preparo}</p>
-                <a href="${receita.url_video}" target="_blank">Assistir vídeo</a>
+                <a href="${receita.url_video}" target="_blank"><p>Assistir vídeo</p></a>
              `;
             
             receitasDiv.appendChild(receitaElement);
